3727d8f3c688e7a6bf975c9ce8f275272eb76103
Mariangela Crispim <mmcrispim@yahoo.com.br>
2024-03-18 18:49:22 -0300

    criação de variáveis e saída destes valores

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+# crie um comando que o usuario digita uma string e retorna o tamanho dela
+
+# print(len(input("digite aqui a string: ")))
+
+primeiro = int(input("Digite o primeiro número: "))
+segundo = int(input("Digite o segundo número: "))
+
+print(primeiro + segundo)
