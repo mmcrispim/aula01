af92b393a936ca8cc3c5157a954070b80ca61107
Mariangela Crispim <mmcrispim@yahoo.com.br>
2024-03-22 18:05:31 -0300

    Desafio da aula 01

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ print("Hello World")
 
 # print(len(input("digite aqui a string: ")))
 
-primeiro = int(input("Digite o primeiro número: "))
-segundo = int(input("Digite o segundo número: "))
+# primeiro = int(input("Digite o primeiro número: "))
+# segundo = int(input("Digite o segundo número: "))
 
-print(primeiro + segundo)
+# print(primeiro + segundo)
+
+CONSTANTE_BONUS = 1000
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite seu bônus: "))
+
+
+valor_final = CONSTANTE_BONUS + (salario * bonus)
+
+# print(valor_final)
+print(f"O Usuário {nome} possui o Bônus {valor_final}")
